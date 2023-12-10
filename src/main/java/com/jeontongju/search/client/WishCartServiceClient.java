5d3476505fd2d54a9da40b46dffadd06d0efc5c4
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-12-10 18:38:17 +0900

    FEAT
    add circuit breaker
    
    SCRUM-32

--- src/main/java/com/jeontongju/search/client/WishCartServiceClient.java
+++ src/main/java/com/jeontongju/search/client/WishCartServiceClient.java
@@ -1,16 +1,33 @@
 package com.jeontongju.search.client;
 
 import com.jeontongju.search.dto.request.IsWishProductDto;
-import com.jeontongju.search.dto.response.IsWishInfoDto;
 import com.jeontongju.search.dto.temp.FeignFormat;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.HashMap;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "wish-cart-service")
 public interface WishCartServiceClient {
 
+  @CircuitBreaker(
+          name = "wishCartServiceClient@getIsWish",
+          fallbackMethod = "getIsWishFallbackMethod"
+  )
   @PostMapping("/products/likes")
   FeignFormat<HashMap<String, Boolean>> getIsWish(@RequestBody IsWishProductDto isWishProductDto);
+
+  default FeignFormat<HashMap<String, Boolean>> getIsWishFallbackMethod(@RequestBody IsWishProductDto isWishProductDto, Throwable t) {
+
+    HashMap<String, Boolean> result = new HashMap<>();
+    isWishProductDto.getProductIds().stream().forEach(id -> result.put(id, false));
+
+    return FeignFormat.<HashMap<String, Boolean>>builder()
+            .code(HttpStatus.OK.value())
+            .data(result)
+            .build();
+
+  }
 }
